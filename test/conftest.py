5615df6288b07c493e0e395ea569e9f3f12364d6
shadeMe <shadeMe@users.noreply.github.com>
2024-05-15 15:04:30 +0200

    build: Fix project structure and version

--- test/conftest.py
+++ test/conftest.py
@@ -2,12 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from pathlib import Path
-from test.tracing.utils import SpyingTracer
-from typing import Generator
 
 import pytest
 
-from haystack import tracing
 from haystack.testing.test_utils import set_all_seeds
 
 set_all_seeds(0)
@@ -16,31 +13,3 @@ set_all_seeds(0)
 @pytest.fixture()
 def test_files_path():
     return Path(__file__).parent / "test_files"
-
-
-@pytest.fixture(autouse=True)
-def request_blocker(request: pytest.FixtureRequest, monkeypatch):
-    """
-    This fixture is applied automatically to all tests.
-    Those that are not marked as integration will have the requests module
-    monkeypatched to avoid making HTTP requests by mistake.
-    """
-    marker = request.node.get_closest_marker("integration")
-    if marker is not None:
-        return
-
-    def urlopen_mock(self, method, url, *args, **kwargs):
-        raise RuntimeError(f"The test was about to {method} {self.scheme}://{self.host}{url}")
-
-    monkeypatch.setattr("urllib3.connectionpool.HTTPConnectionPool.urlopen", urlopen_mock)
-
-
-@pytest.fixture()
-def spying_tracer() -> Generator[SpyingTracer, None, None]:
-    tracer = SpyingTracer()
-    tracing.enable_tracing(tracer)
-
-    yield tracer
-
-    # Make sure to disable tracing after the test to avoid affecting other tests
-    tracing.disable_tracing()
