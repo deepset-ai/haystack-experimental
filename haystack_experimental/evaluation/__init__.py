d44f09a4870863d78d2a627b233413b2985825db
shadeMe <shadeMe@users.noreply.github.com>
2024-05-22 11:46:17 +0200

    Sort imports

--- haystack_experimental/evaluation/__init__.py
+++ haystack_experimental/evaluation/__init__.py
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from .eval_harness import EvaluationHarness, EvalRunOverrides
+from .eval_harness import EvalRunOverrides, EvaluationHarness
 
 _all_ = ["EvaluationHarness", "EvalRunOverrides"]
