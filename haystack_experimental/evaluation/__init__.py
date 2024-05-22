57ece86147bb6b138cde90ace173bcdc496dec40
Madeesh Kannan <shadeMe@users.noreply.github.com>
2024-05-22 11:57:17 +0200

    feat: Implement abstract `EvaluationHarness` class (#5)
    * feat: Implement abstract `EvaluationHarness` class
    
    * Add license header
    
    * docstring lint
    
    * Sort imports
    
    * Lint

--- haystack_experimental/evaluation/__init__.py
+++ haystack_experimental/evaluation/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+
+from .eval_harness import EvalRunOverrides, EvaluationHarness
+
+_all_ = ["EvaluationHarness", "EvalRunOverrides"]
