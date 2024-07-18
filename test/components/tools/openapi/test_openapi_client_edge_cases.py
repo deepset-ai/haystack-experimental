e5841a71ef364eddeeedb0f3e0fd13ca4a454d2a
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-18 17:57:30 +0200

    Improve OpenAPITool corner cases handling (missing operationId, servers under paths, etc) (#37)
    * Improve corner cases handling
    
    * Add unit tests for server order resolution and missing operationId

--- test/components/tools/openapi/test_openapi_client_edge_cases.py
+++ test/components/tools/openapi/test_openapi_client_edge_cases.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from haystack_experimental.components.tools.openapi._openapi import OpenAPIServiceClient, ClientConfiguration
+from haystack_experimental.components.tools.openapi._openapi import ClientConfiguration, OpenAPIServiceClient
 from test.components.tools.openapi.conftest import FastAPITestClient, create_openapi_spec
 
 
@@ -26,4 +26,29 @@ class TestEdgeCases:
         with pytest.raises(ValueError, match="No operation found with operationId"):
             client.invoke(payload)
 
-    # TODO: Add more tests for edge cases
+    def test_missing_operation_id_in_operation(self, test_files_path):
+        """
+        Test that the tool definition is generated correctly when the operationId is missing in the specification.
+        """
+        config = ClientConfiguration(openapi_spec=create_openapi_spec(test_files_path / "yaml" / "openapi_edge_cases.yml"),
+                                     request_sender=FastAPITestClient(None))
+
+        tools = config.get_tools_definitions(),
+        tool_def = tools[0][0]
+        assert tool_def["type"] == "function"
+        assert tool_def["function"]["name"] == "missing-operation-id_get"
+
+    def test_servers_order(self, test_files_path):
+        """
+        Test that servers defined in different locations in the specification are used correctly.
+        """
+
+        config = ClientConfiguration(openapi_spec=create_openapi_spec(test_files_path / "yaml" / "openapi_edge_cases.yml"),
+                                     request_sender=FastAPITestClient(None))
+
+        op = config.openapi_spec.find_operation_by_id("servers-order-path")
+        assert op.get_server() == "https://inpath.example.com"
+        op = config.openapi_spec.find_operation_by_id("servers-order-operation")
+        assert op.get_server() == "https://inoperation.example.com"
+        op = config.openapi_spec.find_operation_by_id("missing-operation-id_get")
+        assert op.get_server() == "http://localhost"
