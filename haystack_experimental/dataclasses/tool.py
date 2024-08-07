0a13dcefd97607543718e30613961156c93cc1fb
anakin87 <stefanofiorucci@gmail.com>
2024-08-07 19:26:45 +0200

    progress + tests

--- haystack_experimental/dataclasses/tool.py
+++ haystack_experimental/dataclasses/tool.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from dataclasses import asdict, dataclass
 from typing import Any, Callable, Dict
 
@@ -8,6 +12,18 @@ from jsonschema.exceptions import SchemaError
 
 @dataclass
 class Tool:
+    """
+    Data class representing a tool for which Language Models can prepare a call.
+
+    Accurate definitions of the textual attributes such as `name` and `description`
+    are important for the Language Model to correctly prepare the call.
+
+    :param name: Name of the tool.
+    :param description: Description of the tool.
+    :param parameters: A JSON schema defining the parameters expected by the tool.
+    :param function: The callable that will be invoked when the tool is called.
+    """
+
     name: str
     description: str
     parameters: Dict[str, Any]
@@ -21,20 +37,40 @@ class Tool:
             raise ValueError("The provided parameters do not define a valid JSON schema") from e
 
     @property
-    def tool_spec(self)-> Dict[str, Any]:
-        """Get the tool specification."""
+    def tool_spec(self) -> Dict[str, Any]:
+        """
+        Return the tool specification to be used by the Language Model.
+        """
         return {"name": self.name, "description": self.description, "parameters": self.parameters}
 
-    def invoke(self, **kwargs):
-        """Invoke the tool."""
+    def invoke(self, **kwargs) -> Any:
+        """
+        Invoke the tool with the provided keyword arguments.
+        """
         return self.function(**kwargs)
 
     def to_dict(self) -> Dict[str, Any]:
+        """
+        Convert the Tool to a dictionary.
+
+        :returns:
+            Serialized version of the Tool.
+        """
+
         serialized = asdict(self)
         serialized["function"] = serialize_callable(self.function)
         return serialized
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "Tool":
+        """
+        Create a Tool from a dictionary.
+
+        :param data:
+            The serialized version of the Tool.
+
+        :returns:
+            The deserialized Tool.
+        """
         data["function"] = deserialize_callable(data["function"])
         return cls(**data)
