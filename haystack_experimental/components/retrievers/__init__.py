8cd5d31df261760c612ddd089b943acf7d1c4a68
Vladimir Blagojevic <dovlex@gmail.com>
2024-08-09 12:08:46 +0100

    feat: Add ChatMessageRetriever (#50)
    * Add ChatMessageRetriever
    
    * Update
    
    * Update test
    
    * Add pipeline serde test
    
    ---------
    
    Co-authored-by: Julian Risch <julian.risch@deepset.ai>

---
+++ haystack_experimental/components/retrievers/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from haystack_experimental.components.retrievers.chat_message_retriever import ChatMessageRetriever
+
+_all_ = ["ChatMessageRetriever"]
