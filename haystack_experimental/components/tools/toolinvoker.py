409b19f2d883da7ab1fdb548eaa7b11b76981bb7
anakin87 <stefanofiorucci@gmail.com>
2024-08-14 18:28:39 +0200

    improvements

--- haystack_experimental/components/tools/toolinvoker.py
+++ haystack_experimental/components/tools/toolinvoker.py
@@ -6,8 +6,9 @@ import json
 from typing import List
 
 from haystack import component
-from haystack_experimental.dataclasses import ChatMessage
-from haystack_experimental.dataclasses.tool import Tool
+
+from haystack_experimental.dataclasses import ChatMessage, ChatRole
+from haystack_experimental.dataclasses.tool import Tool, ToolInvocationError
 
 _FUNCTION_NAME_FAILURE = (
     "I'm sorry, I tried to run a function that did not exist. Would you like me to correct it and try again?"
@@ -15,44 +16,39 @@ _FUNCTION_NAME_FAILURE = (
 _FUNCTION_RUN_FAILURE = "Seems there was an error while running the function: {error}"
 
 
-# JUST A SLIGHTLY MODIFIED VERSION OF OpenAIFunctionCaller
 @component
 class ToolInvoker:
-    """
-    OpenAIFunctionCaller processes a list of chat messages and call tools if encounters a tool call.
-    """
-
     def __init__(self, tools: List[Tool], raise_on_failure: bool = False):
         self.tools = tools
         self._tool_names = {tool.name for tool in tools}
         self.raise_on_failure = raise_on_failure
 
-    @component.output_types(tool_messages=List[ChatMessage])
-    def run(self, messages: List[ChatMessage]):
-        """
-        Process a list of chat messages and call tools if encounters a tool call.
-        """
-        replies = []
+    @component.output_types(tool_results=List[ChatMessage])
+    def run(self, tool_message: ChatMessage):
+        tool_results = []
 
-        tool_calls = messages[-1].tool_calls
-        if tool_calls:
-            for tool_call in tool_calls:
-                tool_name = tool_call.tool_name
-                tool_arguments = tool_call.arguments
-                if tool_name in self._tool_names:
-                    tool_to_call = next(tool for tool in self.tools if tool.name == tool_name)
-                    try:
-                        tool_response = tool_to_call.invoke(**tool_arguments)
-                        replies.append(
-                            ChatMessage.from_tool(content=json.dumps(tool_response), tool_call_id=tool_call.id)
-                        )
-                    # pylint: disable=broad-exception-caught
-                    except Exception as e:
-                        replies.append(
-                            ChatMessage.from_tool(
-                                content=_FUNCTION_RUN_FAILURE.format(error=e), tool_call_id=tool_call.id
-                            )
-                        )
-                else:
-                    replies.append(ChatMessage.from_tool(_FUNCTION_NAME_FAILURE, tool_call_id=tool_call.id))
-        return {"tool_messages": replies}
+        if not tool_message.is_from(ChatRole.ASSISTANT):
+            raise ValueError("ToolInvoker only supports messages from the assistant role.")
+
+        tool_calls = tool_message.tool_calls
+        if not tool_calls:
+            raise ValueError("ToolInvoker only supports messages with tool calls.")
+
+        for tool_call in tool_calls:
+            tool_name = tool_call.tool_name
+            tool_arguments = tool_call.arguments
+            if tool_name in self._tool_names:
+                tool_to_call = next(tool for tool in self.tools if tool.name == tool_name)
+                try:
+                    tool_response = tool_to_call.invoke(**tool_arguments)
+                    tool_results.append(ChatMessage.from_tool(tool_result=json.dumps(tool_response), origin=tool_call))
+
+                except ToolInvocationError as e:
+                    if self.raise_on_failure:
+                        raise e
+                    tool_results.append(
+                        ChatMessage.from_tool(tool_result=_FUNCTION_RUN_FAILURE.format(error=e), origin=tool_call)
+                    )
+            else:
+                raise ValueError(f"Tool {tool_name} not found in the list of tools.")
+        return {"tool_results": tool_results}
