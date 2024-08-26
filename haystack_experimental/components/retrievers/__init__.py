f7eeb91a9b79ab85286ee7409f525417b56ad7a8
David S. Batista <dsbatista@gmail.com>
2024-08-26 15:34:39 +0200

    feat: adding an hierarchical document builder and a auto-merge retriever (#56)
    * initial import
    
    * adding LICENSE header
    
    * fixing tests for experimental package
    
    * fixing tests for experimental package
    
    * fixing tests for experimental package
    
    * linting issues
    
    * attending PR comments
    
    * adding serialization and deserialization
    
    * adding serialization and deserialization
    
    * fixing tests
    
    * Update haystack_experimental/components/retrievers/auto_merging_retriever.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update test/components/splitters/test_hierarchical_doc_splitter.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * attending PR comments
    
    * attending PR comments
    
    * fixing tests
    
    * improving test coverage
    
    * improving test coverage
    
    * warning for unsupported doc stores
    
    * fix for unsupported doc stores
    
    * adding dependencies for tests
    
    * trying another approach: adding dependencies for tests
    
    * missed using fixture
    
    * moving all inside the test
    
    * fixing unsupported DBs test
    
    * updating tests
    
    * removing unused imports
    
    * remove import patching
    
    * nit
    
    * Update haystack_experimental/components/retrievers/auto_merging_retriever.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * Update haystack_experimental/components/splitters/hierarchical_doc_splitter.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * docstrings
    
    * docstrings
    
    * fixing tests
    
    * docstrings
    
    * improving tests
    
    * adding checks/safeguards
    
    * adding checks/safeguards for not supported DocumentStores
    
    * cleaning
    
    * removing old checks
    
    * fixing docstrings
    
    * fixing linting and tests
    
    * cleaning tests
    
    * cleaning
    
    * adding pydoc
    
    * fix pydocs
    
    * fix pydocs
    
    * DocumentSplitter part of the object, created only once
    
    * adding serdes to the retriever
    
    ---------
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>

--- haystack_experimental/components/retrievers/__init__.py
+++ haystack_experimental/components/retrievers/__init__.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from haystack_experimental.components.retrievers.auto_merging_retriever import AutoMergingRetriever
 from haystack_experimental.components.retrievers.chat_message_retriever import ChatMessageRetriever
 
-_all_ = ["ChatMessageRetriever"]
+_all_ = ["AutoMergingRetriever", "ChatMessageRetriever"]
