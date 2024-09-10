a184ac46e4778078e938f51aa68b2672ec8bb88b
Vladimir Blagojevic <dovlex@gmail.com>
2024-09-10 10:50:46 +0100

    feat: Adds `last_k` parameter to `ChatMessageRetriever` init/run methods (#68)
    * Initial implementation
    
    * Add top_k as init param as well
    
    * Improve docs
    
    * More unit tests
    
    * Rename top_k to last_k

--- haystack_experimental/components/retrievers/chat_message_retriever.py
+++ haystack_experimental/components/retrievers/chat_message_retriever.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from haystack import DeserializationError, component, default_from_dict, default_to_dict, logging
 from haystack.core.serialization import import_class_by_name
@@ -39,14 +39,19 @@ class ChatMessageRetriever:
     ```
     """
 
-    def __init__(self, message_store: ChatMessageStore):
+    def __init__(self, message_store: ChatMessageStore, last_k: int = 10):
         """
         Create the ChatMessageRetriever component.
 
         :param message_store:
             An instance of a ChatMessageStore.
+        :param last_k:
+            The number of last messages to retrieve. Defaults to 10 messages if not specified.
         """
         self.message_store = message_store
+        if last_k <= 0:
+            raise ValueError(f"last_k must be greater than 0. Currently, the last_k is {last_k}")
+        self.last_k = last_k
 
     def to_dict(self) -> Dict[str, Any]:
         """
@@ -56,7 +61,7 @@ class ChatMessageRetriever:
             Dictionary with serialized data.
         """
         message_store = self.message_store.to_dict()
-        return default_to_dict(self, message_store=message_store)
+        return default_to_dict(self, message_store=message_store, last_k=self.last_k)
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "ChatMessageRetriever":
@@ -84,11 +89,20 @@ class ChatMessageRetriever:
         return default_from_dict(cls, data)
 
     @component.output_types(messages=List[ChatMessage])
-    def run(self):
+    def run(self, last_k: Optional[int] = None):
         """
         Run the ChatMessageRetriever
 
+        :param last_k: The number of last messages to retrieve. This parameter takes precedence over the last_k
+            parameter passed to the ChatMessageRetriever constructor. If unspecified, the last_k parameter passed
+            to the constructor will be used.
         :returns:
             - `messages` - The retrieved chat messages.
+        :raises ValueError: If last_k is not None and is less than 1
         """
-        return {"messages": self.message_store.retrieve()}
+        if last_k is not None and last_k <= 0:
+            raise ValueError("last_k must be greater than 0")
+
+        last_k = last_k or self.last_k
+
+        return {"messages": self.message_store.retrieve()[-last_k:]}
