2a7a0b9a5d3434ea85a31ecc3812102982924bb8
David S. Batista <dsbatista@gmail.com>
2024-09-23 13:43:44 +0200

    initial import

---
+++ haystack_experimental/components/extractors/graph_rag.py
@@ -0,0 +1,12 @@
+
+from haystack import component
+
+@component
+class RelationshipExtractor:
+    """
+    Extract relationships between entities from a text.
+
+    Simple and fast rule-based approach to extract relationships between entities from a text based on
+    part-of-speech tags and dependency parsing.
+    """
+    pass
