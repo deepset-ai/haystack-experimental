779cba829f6089e80973a0470dfe389619ad3987
Madeesh Kannan <shadeMe@users.noreply.github.com>
2024-10-09 10:52:15 +0200

    feat: Add async support to `OpenSearchBM25Retreiver` (#116)

---
+++ haystack_experimental/components/retrievers/opensearch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .bm25_retriever import OpenSearchBM25Retriever
+
+__all__ = ["OpenSearchBM25Retriever"]
