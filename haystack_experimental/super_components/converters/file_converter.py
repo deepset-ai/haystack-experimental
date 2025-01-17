05b36354bc395fa33a5a04daca6482f5ece65f11
mathislucka <mathis.lucka@gmail.com>
2025-01-17 10:36:36 +0100

    WIP extend MultiFileConverter

--- haystack_experimental/super_components/converters/file_converter.py
+++ haystack_experimental/super_components/converters/file_converter.py
@@ -1,14 +1,24 @@
-from typing import List, Dict
+from typing import Any, List, Dict, Optional
 
-from haystack.components.converters import TextFileToDocument, DOCXToDocument, XLSXToDocument, PyPDFToDocument
+from haystack.components.converters import (
+    CSVToDocument,
+    TextFileToDocument,
+    DOCXToDocument,
+    XLSXToDocument,
+    PyPDFToDocument,
+    PPTXToDocument,
+    JSONConverter,
+    HTMLToDocument,
+    AzureOCRDocumentConverter,
+    MarkdownToDocument
+)
+
 from haystack.components.routers import FileTypeRouter
-
 from haystack import component, Pipeline
-
 from haystack.core.component import Component
-
 from haystack_experimental.components.wrappers.pipeline_wrapper import PipelineWrapper
 
+from enum import StrEnum
 from dataclasses import dataclass
 
 import inspect
@@ -32,11 +42,41 @@ class ComponentModule:
 
 
 
+class ConverterMimeType(StrEnum):
+    CSV = "text/csv"
+    DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    HTML = "text/html"
+    JSON = "application/json"
+    MD = "text/markdown"
+    TEXT = "text/plain"
+    PDF = "application/pdf"
+    PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+
 _FILE_CONVERTER_MODULES = {
-    "text/plain": ComponentModule(component=TextFileToDocument),
-    "application/pdf": ComponentModule(component=PyPDFToDocument),
-    # ...
+    ConverterMimeType.CSV: ComponentModule(component=CSVToDocument),
+    ConverterMimeType.DOCX: ComponentModule(component=DOCXToDocument),
+    ConverterMimeType.HTML: ComponentModule(component=HTMLToDocument),
+    ConverterMimeType.JSON: ComponentModule(component=JSONConverter, config_mapping={"json_content_key": "content_key"}),
+    ConverterMimeType.MD: ComponentModule(component=MarkdownToDocument),
+    ConverterMimeType.TEXT: ComponentModule(component=TextFileToDocument),
+    ConverterMimeType.PDF: ComponentModule(component=PyPDFToDocument),
+    ConverterMimeType.PPTX: ComponentModule(component=PPTXToDocument),
+    ConverterMimeType.XLSX: ComponentModule(component=XLSXToDocument)
 }
+
+def _add_modules_to_pipeline(pipeline: Pipeline, modules: List[ComponentModule], component_args: Dict[str, Any]) -> None:
+    for module in modules:
+        comp = module.component
+        name = module.name
+        config = {}
+        for param, mapped_param in module.config_mapping.items():
+            if param in component_args:
+                config[mapped_param] = component_args[param]
+
+        pipeline.add_component(name, comp(**config))
+
 
 
 
@@ -51,26 +91,18 @@ class MultiFileConverter(PipelineWrapper):
     converter.run(sources=["test.txt", "test.pdf"], meta={})
     ```
     """
-    def __init__(self, mime_types: List[str] = None):
+    def __init__(self, mime_types: List[ComponentModule] = None, json_content_key: str = "content") -> None:
         if mime_types is None:
             self.mime_types = list(_FILE_CONVERTER_MODULES.keys())
         else:
             self.mime_types = mime_types
 
         args = locals()
-
         pp = Pipeline()
-        for mime_type in self.mime_types:
-            module = _FILE_CONVERTER_MODULES[mime_type]
-            config = {}
-            for param, mapped_param in module.config_mapping.items():
-                if param in args:
-                    config[mapped_param] = args[param]
-
-            pp.add_component(module.name, module.component(**config))
+        converter_modules = [_FILE_CONVERTER_MODULES[mime_type] for mime_type in self.mime_types]
+        _add_modules_to_pipeline(pp, converter_modules, args)
 
         router = FileTypeRouter(mime_types=self.mime_types)
-
         pp.add_component("router", router)
 
         for mime_type in self.mime_types:
