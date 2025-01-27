36946a2a69f1d82caee92e46ff140eb9d32dc66d
Abraham Yusuf <abraham.yusuf@deepset.ai>
2025-01-27 14:36:12 +0100

    chore: add license headers

--- haystack_experimental/super_components/indexers/__init__.py
+++ haystack_experimental/super_components/indexers/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from haystack_experimental.super_components.indexers.document_indexer import DocumentIndexer
 
 __all__ = [
