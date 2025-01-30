fa83fb31b31b0abcda72059f7a82ffd4516fc20d
mathislucka <mathis.lucka@gmail.com>
2025-01-30 10:50:26 +0100

    feat: add sync run method

--- haystack_experimental/core/pipeline/async_pipeline.py
+++ haystack_experimental/core/pipeline/async_pipeline.py
@@ -478,3 +478,116 @@ class AsyncPipeline(PipelineBase):
         ):
             final = partial
         return final or {}
+
+    def run(
+            self,
+            data: Dict[str, Any],
+            include_outputs_from: Optional[Set[str]] = None,
+            concurrency_limit: int = 4,
+    ) -> Dict[str, Any]:
+        """
+        Runs the pipeline with given input data.
+
+        This method is synchronous, but it runs components asynchronously internally.
+        Check out `run_async` or `run_async_generator` if you are looking for async-methods.
+
+        Usage:
+        ```python
+        from haystack import Document
+        from haystack.utils import Secret
+        from haystack.document_stores.in_memory import InMemoryDocumentStore
+        from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
+        from haystack.components.generators import OpenAIGenerator
+        from haystack.components.builders.answer_builder import AnswerBuilder
+        from haystack.components.builders.prompt_builder import PromptBuilder
+
+        from haystack_experimental import AsyncPipeline
+
+        # Write documents to InMemoryDocumentStore
+        document_store = InMemoryDocumentStore()
+        document_store.write_documents([
+            Document(content="My name is Jean and I live in Paris."),
+            Document(content="My name is Mark and I live in Berlin."),
+            Document(content="My name is Giorgio and I live in Rome.")
+        ])
+
+        prompt_template = \"\"\"
+        Given these documents, answer the question.
+        Documents:
+        {% for doc in documents %}
+            {{ doc.content }}
+        {% endfor %}
+        Question: {{question}}
+        Answer:
+        \"\"\"
+
+        retriever = InMemoryBM25Retriever(document_store=document_store)
+        prompt_builder = PromptBuilder(template=prompt_template)
+        llm = OpenAIGenerator(api_key=Secret.from_token(api_key))
+
+        rag_pipeline = AsyncPipeline()
+        rag_pipeline.add_component("retriever", retriever)
+        rag_pipeline.add_component("prompt_builder", prompt_builder)
+        rag_pipeline.add_component("llm", llm)
+        rag_pipeline.connect("retriever", "prompt_builder.documents")
+        rag_pipeline.connect("prompt_builder", "llm")
+
+        # Ask a question
+        question = "Who lives in Paris?"
+
+
+        async def run_inner(data, include_outputs_from):
+            return await rag_pipeline.run_async(data=data, include_outputs_from=include_outputs_from)
+
+        data = {
+            "retriever": {"query": question},
+            "prompt_builder": {"question": question},
+        }
+
+        results = rag_pipeline.run(data)
+
+        print(results["llm"]["replies"])
+        # Jean lives in Paris
+        ```
+
+        :param data:
+            A dictionary of inputs for the pipeline's components. Each key is a component name
+            and its value is a dictionary of that component's input parameters:
+            ```
+            data = {
+                "comp1": {"input1": 1, "input2": 2},
+            }
+            ```
+            For convenience, this format is also supported when input names are unique:
+            ```
+            data = {
+                "input1": 1, "input2": 2,
+            }
+            ```
+        :param include_outputs_from:
+            Set of component names whose individual outputs are to be
+            included in the pipeline's output. For components that are
+            invoked multiple times (in a loop), only the last-produced
+            output is included.
+        :param concurrency_limit: The maximum number of components that should be allowed to run concurrently.
+        :returns:
+            A dictionary where each entry corresponds to a component name
+            and its output. If `include_outputs_from` is `None`, this dictionary
+            will only contain the outputs of leaf components, i.e., components
+            without outgoing connections.
+
+        :raises ValueError:
+            If invalid inputs are provided to the pipeline.
+        :raises PipelineRuntimeError:
+            If the Pipeline contains cycles with unsupported connections that would cause
+            it to get stuck and fail running.
+            Or if a Component fails or returns output in an unsupported type.
+        :raises PipelineMaxComponentRuns:
+            If a Component reaches the maximum number of times it can be run in this Pipeline.
+        """
+        return asyncio.run(
+            self.run_async(
+                data=data,
+                include_outputs_from=include_outputs_from,
+                concurrency_limit=concurrency_limit
+            ))
