144cde8c971ffad6116def699b71db3e17a84d07
mathislucka <mathis.lucka@gmail.com>
2025-01-31 10:23:05 +0100

    add init

---
+++ haystack_experimental/super_components/converters/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from haystack_experimental.super_components.converters.file_converter import AutoFileConverter
+
+_all_ = ["AutoFileConverter"]
