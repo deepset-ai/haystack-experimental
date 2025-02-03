0619f95b7562f114b65c89b1bf9a40aeba3877c3
mathislucka <mathis.lucka@gmail.com>
2025-02-03 11:04:11 +0100

    test: test for pipeline reentrance

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
+from test.tracing.utils import SpyingTracer
+
 from haystack.testing.test_utils import set_all_seeds
+from haystack import tracing
 
 set_all_seeds(0)
 
@@ -13,3 +17,13 @@ set_all_seeds(0)
 @pytest.fixture()
 def test_files_path():
     return Path(__file__).parent / "test_files"
+
+@pytest.fixture()
+def spying_tracer() -> Generator[SpyingTracer, None, None]:
+    tracer = SpyingTracer()
+    tracing.enable_tracing(tracer)
+
+    yield tracer
+
+    # Make sure to disable tracing after the test to avoid affecting other tests
+    tracing.disable_tracing()
---
+++ test/core/pipeline/test_async_pipeline.py
@@ -0,0 +1,47 @@
+from typing import Dict
+
+import asyncio
+import time
+
+from haystack import component
+
+from haystack_experimental import AsyncPipeline
+
+@component
+class Waiter:
+    @component.output_types(waited_for=int)
+    def run(self, wait_for: int) -> Dict[str, int]:
+        time.sleep(wait_for)
+        return {'waited_for': wait_for}
+
+    @component.output_types(waited_for=int)
+    async def run_async(self, wait_for: int) -> Dict[str, int]:
+        await asyncio.sleep(wait_for)
+        return {'waited_for': wait_for}
+
+
+def test_async_pipeline_reentrance(spying_tracer):
+    pp = AsyncPipeline()
+    pp.add_component("wait", Waiter())
+
+    run_data = [
+        {"wait_for": 2},
+        {"wait_for": 3},
+    ]
+
+    async_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(async_loop)
+
+    async def run_all():
+        # Create concurrent tasks for each pipeline run
+        tasks = [pp.run_async(data) for data in run_data]
+        await asyncio.gather(*tasks)
+
+    try:
+        async_loop.run_until_complete(run_all())
+        component_spans = [sp for sp in spying_tracer.spans if sp.operation_name == "haystack.component.run_async"]
+        for span in component_spans:
+            assert span.tags["haystack.component.visits"] == 1
+    finally:
+        async_loop.close()
+
