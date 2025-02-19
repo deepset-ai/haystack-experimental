f3dc5136c8083d43f082b52ee4a5bd7b9fae7f9d
mathislucka <mathis.lucka@gmail.com>
2025-02-19 17:39:07 +0100

    chore: reduce diff

--- examples/agent_prompts/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-from .system_prompt import issue_prompt
-from .repo_viewer_tool import repo_viewer_prompt, repo_viewer_schema
-
-_all_ = ["issue_prompt", "repo_viewer_prompt", "repo_viewer_schema"]
