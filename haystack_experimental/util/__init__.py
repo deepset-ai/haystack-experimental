573169cfecc1dce04b562dd3d0d5d28d90dea315
Sebastian Husch Lee <sjrl423@gmail.com>
2025-03-11 10:43:15 +0100

    Remove

--- haystack_experimental/util/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-from haystack_experimental.util.asynchronous import is_callable_async_compatible
-
-__all__ = ["is_callable_async_compatible"]
-
