ef82582227d1bbda5d7f4c58ff31712448180cc3
David S. Batista <dsbatista@gmail.com>
2025-06-18 18:26:17 +0200

    formating and renaming file

--- haystack_experimental/dataclasses/breakpoints.py
+++ haystack_experimental/dataclasses/breakpoints.py
@@ -1,5 +1,10 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from dataclasses import dataclass
 from typing import Optional, Set, Union
+
 
 @dataclass
 class Breakpoint:
@@ -8,7 +13,7 @@ class Breakpoint:
     """
     component_name: str
     visit_count: int
-    
+
 
     def __hash__(self):
         return hash((self.component_name, self.visit_count))
@@ -17,7 +22,7 @@ class Breakpoint:
         if not isinstance(other, Breakpoint):
             return False
         return self.component_name == other.component_name and self.visit_count == other.visit_count
-    
+
     def __str__(self):
             return f"Breakpoint(component_name={self.component_name}, visit_count={self.visit_count})"
 
@@ -41,12 +46,14 @@ class ToolBreakpoint(Breakpoint):
         if not isinstance(other, ToolBreakpoint):
             return False
         return super().__eq__(other)
-    
+
     def __str__(self):
         if self.tool_name:
-            return f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, tool_name={self.tool_name})"
+            return (f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, "
+                    f"tool_name={self.tool_name})")
         else:
-            return f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, tool_name=ALL_TOOLS)"
+            return (f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, "
+                    f"tool_name=ALL_TOOLS)")
 
     def __repr__(self):
         return self.__str__()
@@ -67,12 +74,13 @@ class AgentBreakpoint:
         self.breakpoints = breakpoints
 
     def add_breakpoint(self, breakpoint: Union[Breakpoint,ToolBreakpoint]):
+        """
+        Adds a breakpoint to the set of breakpoints.
+        """
         self.breakpoints.add(breakpoint)
 
     def remove_breakpoint(self, breakpoint: Union[Breakpoint,ToolBreakpoint]):
+        """
+        Removes a breakpoint from the set of breakpoints.
+        """
         self.breakpoints.remove(breakpoint)
-
-
-
-
-
