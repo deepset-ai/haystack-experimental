b0234981c5e348fd42e2bbb166cd5ff5f36ff603
David S. Batista <dsbatista@gmail.com>
2025-06-18 18:16:53 +0200

    initial import

---
+++ haystack_experimental/dataclasses/breakpoints_holder.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from typing import Optional, Set, Union
+
+@dataclass
+class Breakpoint:
+    """
+    A dataclass to hold a breakpoint for a component.    
+    """
+    component_name: str
+    visit_count: int
+    
+
+    def __hash__(self):
+        return hash((self.component_name, self.visit_count))
+
+    def __eq__(self, other):
+        if not isinstance(other, Breakpoint):
+            return False
+        return self.component_name == other.component_name and self.visit_count == other.visit_count
+    
+    def __str__(self):
+            return f"Breakpoint(component_name={self.component_name}, visit_count={self.visit_count})"
+
+    def __repr__(self):
+        return self.__str__()
+
+@dataclass
+class ToolBreakpoint(Breakpoint):
+    """
+    A dataclass to hold a breakpoint that can be used to debug a Tool.
+
+    If tool_name is None, it means that the breakpoint is for every tool in the component.
+    Otherwise, it means that the breakpoint is for the tool with the given name.
+    """
+    tool_name: Optional[str] = None
+
+    def __hash__(self):
+        return hash((self.component_name, self.visit_count, self.tool_name))
+
+    def __eq__(self, other):
+        if not isinstance(other, ToolBreakpoint):
+            return False
+        return super().__eq__(other)
+    
+    def __str__(self):
+        if self.tool_name:
+            return f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, tool_name={self.tool_name})"
+        else:
+            return f"ToolBreakpoint(component_name={self.component_name}, visit_count={self.visit_count}, tool_name=ALL_TOOLS)"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+@dataclass
+class AgentBreakpoint:
+    """
+    A dataclass to hold a breakpoint that can be used to debug an Agent.
+
+    It holds a set of breakpoints for the components in the Agent.
+    """
+    breakpoints: Set[Union[Breakpoint,ToolBreakpoint]]
+
+    def __init__(self, breakpoints: Set[Union[Breakpoint,ToolBreakpoint]]):
+        if breakpoints is None:
+            breakpoints = set()
+        self.breakpoints = breakpoints
+
+    def add_breakpoint(self, breakpoint: Union[Breakpoint,ToolBreakpoint]):
+        self.breakpoints.add(breakpoint)
+
+    def remove_breakpoint(self, breakpoint: Union[Breakpoint,ToolBreakpoint]):
+        self.breakpoints.remove(breakpoint)
+
+
+
+
+
