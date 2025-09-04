c9bf4d8d4a2db8a63b688cc77116ec6c84aae9f3
Sebastian Husch Lee <sjrl423@gmail.com>
2025-09-04 08:49:38 +0200

    Ignore license headers of hallucination risk calculator

---
+++ haystack_experimental/utils/hallucination_risk_calculator/__init__.py
@@ -0,0 +1,5 @@
+# ruff: noqa: D103
+# Original code Copyright (c) 2024 Hassana Labs
+# Licensed under the MIT License (see LICENSE-MIT).
+# Modified by deepset, 2025.
+# Licensed under the Apache License, Version 2.0 (see LICENSE-APACHE).
