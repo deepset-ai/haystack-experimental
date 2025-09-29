89d1ca5134780813a3eb91c36ae3a813e46f7215
Sebastian Husch Lee <sjrl423@gmail.com>
2025-09-29 12:14:37 +0200

    Refactoring

--- hitl_breakpoint_example.py
+++ hitl_breakpoint_example.py
@@ -12,11 +12,13 @@ from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.core.errors import BreakpointException
 from haystack.core.pipeline.breakpoint import load_pipeline_snapshot
 from haystack.dataclasses import ChatMessage
+from haystack.dataclasses.breakpoints import AgentSnapshot
 from haystack.dataclasses.breakpoints import AgentBreakpoint, ToolBreakpoint
 from haystack.tools import create_tool_from_function
 from rich.console import Console
 
 from haystack_experimental.components.agents.agent import Agent
+from haystack_experimental.components.agents.human_in_the_loop.dataclasses import ToolExecutionDecision
 from haystack_experimental.components.agents.human_in_the_loop.policies import (
     AlwaysAskPolicy,
 )
@@ -79,7 +81,6 @@ except BreakpointException:
 # NOTE: Opened issue to make it easier to find the latest snapshot: https://github.com/deepset-ai/haystack/issues/9828
 possible_snapshots = [Path("pipeline_snapshots") / f for f in os.listdir(Path("pipeline_snapshots"))]
 latest_snapshot_file = str(max(possible_snapshots, key=os.path.getctime))
-snapshot = load_pipeline_snapshot(latest_snapshot_file)
 
 
 # ----
@@ -93,21 +94,27 @@ snapshot = load_pipeline_snapshot(latest_snapshot_file)
 # - tool_calls: List[ToolCall]
 # - tool_descriptions: Dict[str, str] {"tool_name": "tool_description"}
 
-# Create the list of tool calls to send
-serialized_tool_call_messages = snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["messages"]
-tool_call_messages = [ChatMessage.from_dict(m) for m in serialized_tool_call_messages]
-tool_calls = []
-for msg in tool_call_messages:
-    if msg.tool_calls:
-        tool_calls.extend(msg.tool_calls)
-serialized_tool_calls = [tc.to_dict() for tc in tool_calls]
-# TODO The arguments in serialized_tool_calls are not fully correct. Missing the injection from inputs_from_state.
+def get_tool_calls_and_descriptions(agent_snapshot: AgentSnapshot) -> tuple[list[dict], dict[str, str]]:
+    # Create the list of tool calls to send
+    serialized_tool_call_messages = agent_snapshot.component_inputs["tool_invoker"]["serialized_data"][
+        "messages"
+    ]
+    tool_call_messages = [ChatMessage.from_dict(m) for m in serialized_tool_call_messages]
+    tool_calls = []
+    for msg in tool_call_messages:
+        if msg.tool_calls:
+            tool_calls.extend(msg.tool_calls)
+    serialized_tcs = [tc.to_dict() for tc in tool_calls]
+    # TODO The arguments in serialized_tool_calls are not fully correct. Missing the injection from inputs_from_state.
 
-# Create the list of tools to send
-serialized_tools = snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["tools"]
+    # Create the dict of tool descriptions to send
+    serialized_tools = agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["tools"]
+    tool_descs = {t["data"]["name"]: t["data"]["description"] for t in serialized_tools}
+    return serialized_tcs, tool_descs
 
-# Create the dict of tool descriptions to send
-tool_descriptions = {t["data"]["name"]: t["data"]["description"] for t in serialized_tools}
+
+snapshot = load_pipeline_snapshot(latest_snapshot_file)
+serialized_tool_calls, tool_descriptions = get_tool_calls_and_descriptions(agent_snapshot=snapshot.agent_snapshot)
 
 # ----
 # Step 2.1: Mimic the front-end
@@ -139,97 +146,106 @@ for tc in serialized_tool_calls:
 # Receives:
 # - tool_execution_decisions: List[ToolExecutionDecision]
 # - snapshot_id: str --> Needed to know which snapshot to load
-serialized_chat_messages = snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["messages"]
-new_tool_call_messages = []
-additional_state_messages = []
-for msg in serialized_chat_messages:
-    chat_msg = ChatMessage.from_dict(msg)
-    if not chat_msg.tool_calls:
-        continue
 
-    new_tool_calls = []
-    for tc in chat_msg.tool_calls:
-        ted = next(ted for ted in tool_execution_decisions if (ted.tool_id == tc.id or ted.tool_name == tc.tool_name))
-        if ted.execute:
-            # Covers confirm and modify cases
-            if tc.arguments != ted.final_tool_params:
-                # In the modify case we add a user message explaining the modification otherwise the LLM won't know
-                # why the tool parameters changed and will likely just try and call the tool again with the
-                # original parameters.
-                new_tool_call_messages.append(
-                    ChatMessage.from_user(
-                        text=(
-                            f"The parameters for tool '{tc.tool_name}' were updated by the user to:\n"
-                            f"{ted.final_tool_params}"
+
+def update_snapshot_with_tool_execution_decisions(
+    agent_snapshot: AgentSnapshot, tool_execution_decisions: list[ToolExecutionDecision]
+) -> AgentSnapshot:
+    serialized_chat_messages = agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["messages"]
+    new_tool_call_messages = []
+    additional_state_messages = []
+    for msg in serialized_chat_messages:
+        chat_msg = ChatMessage.from_dict(msg)
+        if not chat_msg.tool_calls:
+            continue
+
+        new_tool_calls = []
+        for tc in chat_msg.tool_calls:
+            ted = next(ted for ted in tool_execution_decisions if (ted.tool_id == tc.id or ted.tool_name == tc.tool_name))
+            if ted.execute:
+                # Covers confirm and modify cases
+                if tc.arguments != ted.final_tool_params:
+                    # In the modify case we add a user message explaining the modification otherwise the LLM won't know
+                    # why the tool parameters changed and will likely just try and call the tool again with the
+                    # original parameters.
+                    new_tool_call_messages.append(
+                        ChatMessage.from_user(
+                            text=(
+                                f"The parameters for tool '{tc.tool_name}' were updated by the user to:\n"
+                                f"{ted.final_tool_params}"
+                            )
                         )
                     )
+                new_tool_calls.append(replace(tc, arguments=ted.final_tool_params))
+            else:
+                # Reject case
+                # We create a tool call and tool call result message pair to put into the chat history of State
+                additional_state_messages.append(
+                    # We can't use dataclasses.replace, so we use from_assistant to create a new message
+                    ChatMessage.from_assistant(
+                        text=chat_msg.text,
+                        meta=chat_msg.meta,
+                        name=chat_msg.name,
+                        tool_calls=[tc],
+                        reasoning=chat_msg.reasoning,
+                    )
                 )
-            new_tool_calls.append(replace(tc, arguments=ted.final_tool_params))
-        else:
-            # Reject case
-            # We create a tool call and tool call result message pair to put into the chat history of State
-            additional_state_messages.append(
-                # We can't use dataclasses.replace, so we use from_assistant to create a new message
+                additional_state_messages.append(
+                    ChatMessage.from_tool(
+                        tool_result=ted.feedback or "",
+                        origin=tc,
+                        error=True,
+                    )
+                )
+
+        # Only add the tool call message if there are any tool calls left (i.e. not all were rejected)
+        if new_tool_calls:
+            new_tool_call_messages.append(
                 ChatMessage.from_assistant(
                     text=chat_msg.text,
                     meta=chat_msg.meta,
                     name=chat_msg.name,
-                    tool_calls=[tc],
+                    tool_calls=new_tool_calls,
                     reasoning=chat_msg.reasoning,
                 )
             )
-            additional_state_messages.append(
-                ChatMessage.from_tool(
-                    tool_result=ted.feedback or "",
-                    origin=tc,
-                    error=True,
-                )
-            )
 
-    # Only add the tool call message if there are any tool calls left (i.e. not all were rejected)
-    if new_tool_calls:
-        new_tool_call_messages.append(
-            ChatMessage.from_assistant(
-                text=chat_msg.text,
-                meta=chat_msg.meta,
-                name=chat_msg.name,
-                tool_calls=new_tool_calls,
-                reasoning=chat_msg.reasoning,
-            )
-        )
+    # Modify the chat history in state to handle the rejection cases
+    # 1. Move the tool call message and tool call result message pairs to right after last user message
+    # 2. Leave all remaining tool call messages (i.e. the ones that were confirmed or modified) at the end of the chat
+    serialized_state = agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["state"]
+    state = State.from_dict(serialized_state)
+    chat_history = state.get("messages")
+    last_user_msg_idx = max(i for i, m in enumerate(chat_history) if m.is_from("user"))
+    new_chat_history = chat_history[: last_user_msg_idx + 1] + additional_state_messages + new_tool_call_messages
+    state.set(key="messages", value=new_chat_history, handler_override=replace_values)
 
-# Modify the chat history in state to handle the rejection cases
-# 1. Move the tool call message and tool call result message pairs to right after last user message
-# 2. Leave all remaining tool call messages (i.e. the ones that were confirmed or modified) at the end of the chat
-serialized_state = snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["state"]
-state = State.from_dict(serialized_state)
-chat_history = state.get("messages")
-last_user_msg_idx = max(i for i, m in enumerate(chat_history) if m.is_from("user"))
-new_chat_history = chat_history[: last_user_msg_idx + 1] + additional_state_messages + new_tool_call_messages
-state.set(key="messages", value=new_chat_history, handler_override=replace_values)
+    # Update the snapshot with the new tool call messages and updated state
+    agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["messages"] = [
+        msg.to_dict() for msg in new_tool_call_messages
+    ]
+    agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["state"] = state.to_dict()
+    return agent_snapshot
 
-# Update the snapshot with the new tool call messages and updated state
-snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["messages"] = [
-    msg.to_dict() for msg in new_tool_call_messages
-]
-snapshot.agent_snapshot.component_inputs["tool_invoker"]["serialized_data"]["state"] = state.to_dict()
 
 # ----
 # Step 3: Restart execution after breakpoint with updated snapshot
 # ----
 # NOTE: We extended the Agent in haystack-experimental to allow running with a snapshot and a breakpoint
+new_agent_snapshot = update_snapshot_with_tool_execution_decisions(
+    agent_snapshot=snapshot.agent_snapshot, tool_execution_decisions=tool_execution_decisions
+)
 result = agent.run(
     # NOTE: Messages are still required but are ignored when passing in a snapshot
     messages=[],
-    snapshot=snapshot.agent_snapshot,
+    snapshot=new_agent_snapshot,
     # This break point shouldn't trigger again, since the agent will exit after the next llm call to give the
     # final answer.
-    # TODO Add a convenience function to return a new breakpoint with an incremented visit count ??
     break_point=replace(
-        snapshot.agent_snapshot.break_point,
+        new_agent_snapshot.break_point,
         break_point=replace(
-            snapshot.agent_snapshot.break_point.break_point,
-            visit_count=snapshot.agent_snapshot.break_point.break_point.visit_count + 1,
+            new_agent_snapshot.break_point.break_point,
+            visit_count=new_agent_snapshot.break_point.break_point.visit_count + 1,
         ),
     ),
 )
