4b18f5737686d79c7f74a40859c588fcfdf11b59
Amna Mubashar <amnahkhan.ak@gmail.com>
2025-12-07 23:37:57 +0500

    Fix linting

--- haystack_experimental/memory_stores/mem0/__init__.py
+++ haystack_experimental/memory_stores/mem0/__init__.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING
 from lazy_imports import LazyImporter
 
 _import_structure = {
-    "memory_stores.mem0.memory_store": ["Mem0MemoryStore"],
+    ".memory_store": ["Mem0MemoryStore"],
 }
 
 sys.modules[__name__] = LazyImporter(name=__name__, module_file=__file__, import_structure=_import_structure)
