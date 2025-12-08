0d0515631e69889c1b5fefd60b9f54f315e09f7d
Amna Mubashar <amnahkhan.ak@gmail.com>
2025-12-08 16:35:42 +0500

    Fixes

--- haystack_experimental/components/agents/agent.py
+++ haystack_experimental/components/agents/agent.py
@@ -52,7 +52,7 @@ from haystack_experimental.components.agents.human_in_the_loop import (
 from haystack_experimental.components.agents.human_in_the_loop.strategies import _process_confirmation_strategies
 from haystack_experimental.components.retrievers import ChatMessageRetriever
 from haystack_experimental.components.writers import ChatMessageWriter
-from haystack_experimental.memory_stores.types import MemoryStore
+from haystack_experimental.memory_stores.types import MemoryStore  # pylint: disable=import-error
 
 
 logger = logging.getLogger(__name__)
