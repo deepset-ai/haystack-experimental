ed4db8b044faa69f9795edcafe4f20d9847faf35
David S. Batista <dsbatista@gmail.com>
2025-12-09 10:58:44 +0100

    fixing Agent tests gpt-5-mini

--- test/components/agents/test_agent.py
+++ test/components/agents/test_agent.py
@@ -181,7 +181,7 @@ class TestAgent:
                 "chat_generator": {
                     "type": "haystack.components.generators.chat.openai.OpenAIChatGenerator",
                     "init_parameters": {
-                        "model": "gpt-5o-mini",
+                        "model": "gpt-5-mini",
                         "streaming_callback": None,
                         "api_base_url": None,
                         "organization": None,
