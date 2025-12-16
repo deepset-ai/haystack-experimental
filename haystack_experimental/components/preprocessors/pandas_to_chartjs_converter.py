5ab8671d31287bc034d87b40a2bada588cfb05f2
Mo Sriha <22803208+medsriha@users.noreply.github.com>
2025-12-16 13:29:17 -0600

    add license

--- haystack_experimental/components/preprocessors/pandas_to_chartjs_converter.py
+++ haystack_experimental/components/preprocessors/pandas_to_chartjs_converter.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import json
 from datetime import date, datetime
 from decimal import Decimal
