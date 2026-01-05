372241f01eeea74007c4eb101d58c27861072483
Sebastian Husch Lee <sjrl423@gmail.com>
2026-01-05 13:04:44 +0100

    Some refactoring of in memory chat message store to follow same structure as document stores

---
+++ haystack_experimental/chat_message_stores/in_memory/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2026-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+from typing import TYPE_CHECKING
+
+from lazy_imports import LazyImporter
+
+_import_structure = {"chat_message_store": ["InMemoryChatMessageStore"]}
+
+if TYPE_CHECKING:
+    from .chat_message_store import InMemoryChatMessageStore as InMemoryChatMessageStore
+else:
+    sys.modules[__name__] = LazyImporter(name=__name__, module_file=__file__, import_structure=_import_structure)
