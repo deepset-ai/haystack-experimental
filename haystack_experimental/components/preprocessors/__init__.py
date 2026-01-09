905f16b0ee70e603345d233992cc43ecb05fd9ba
anakin87 <stefanofiorucci@gmail.com>
2026-01-09 10:09:08 +0100

    more removals

--- haystack_experimental/components/preprocessors/__init__.py
+++ haystack_experimental/components/preprocessors/__init__.py
@@ -8,13 +8,11 @@ from typing import TYPE_CHECKING
 from lazy_imports import LazyImporter
 
 _import_structure = {
-    "embedding_based_document_splitter": ["EmbeddingBasedDocumentSplitter"],
     "md_header_level_inferrer": ["MarkdownHeaderLevelInferrer"],
 }
 
 if TYPE_CHECKING:
-    from .embedding_based_document_splitter import EmbeddingBasedDocumentSplitter
-    from .md_header_level_inferrer import MarkdownHeaderLevelInferrer
+    from .md_header_level_inferrer import MarkdownHeaderLevelInferrer as MarkdownHeaderLevelInferrer
 
 else:
     sys.modules[__name__] = LazyImporter(name=__name__, module_file=__file__, import_structure=_import_structure)
